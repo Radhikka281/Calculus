604ff933c526690cd1a0c55c562dfb8f8def293a
Radhikka281 <152479603+Radhikka281@users.noreply.github.com>
2023-12-02 20:12:37 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Constants
+initial_distance = 1.0  # Initial distance of galaxies
+hubble_constant = 0.1  # Hubble constant (adjust as needed)
+simulation_duration = 10  # Duration of the simulation in arbitrary units
+time_interval = 0.1  # Time interval for each frame in the simulation
+
+# Function to calculate the velocity based on Hubble's law
+def hubble_law(distance):
+    return hubble_constant * distance
+
+# Simulation
+time_points = np.arange(0, simulation_duration, time_interval)
+distances = [initial_distance]
+velocities = [hubble_law(initial_distance)]
+
+for time in time_points[1:]:
+    # Update distance and velocity based on Hubble's law
+    new_distance = distances[-1] + velocities[-1] * time_interval
+    new_velocity = hubble_law(new_distance)
+
+    distances.append(new_distance)
+    velocities.append(new_velocity)
+
+# Plotting
+plt.plot(time_points, distances, label='Galaxy Distance')
+plt.xlabel('Time')
+plt.ylabel('Distance')
+plt.title('Cosmic Expansion Simulation')
+plt.legend()
+plt.grid(True)
+plt.show()

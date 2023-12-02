ec9c0e7b2a0eb919feb2e0859d6dfcbfe9016d1d
Radhikka281 <152479603+Radhikka281@users.noreply.github.com>
2023-12-02 21:47:41 +0530

    calc.py

--- calc.py
+++ calc.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Constants
-initial_distance = 1.0  # Initial distance of galaxies
-hubble_constant = 0.1  # Hubble constant (adjust as needed)
-simulation_duration = 10  # Duration of the simulation in arbitrary units
+initial_distance = 9.9  # Initial distance of galaxies
+hubble_constant = 70  # Hubble constant (adjust as needed)
+simulation_duration = 1000  # Duration of the simulation in arbitrary units
 time_interval = 0.1  # Time interval for each frame in the simulation
 
 # Function to calculate the velocity based on Hubble's law
@@ -26,8 +26,8 @@ for time in time_points[1:]:
 
 # Plotting
 plt.plot(time_points, distances, label='Galaxy Distance')
-plt.xlabel('Time')
-plt.ylabel('Distance')
+plt.xlabel('Time (in sec)')
+plt.ylabel('Distance (in lightyear)')
 plt.title('Cosmic Expansion Simulation')
 plt.legend()
 plt.grid(True)
